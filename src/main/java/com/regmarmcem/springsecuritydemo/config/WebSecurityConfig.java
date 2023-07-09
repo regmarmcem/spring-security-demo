dfbcc4e171bb0df3c245fa054bcb192fe1f2f6e3
regmarmcem <tknktknktknktknktknk@gmail.com>
2023-07-09 12:04:14 +0900

    [feature] login page

---
+++ src/main/java/com/regmarmcem/springsecuritydemo/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.regmarmcem.springsecuritydemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authz -> 
+        authz.requestMatchers("/hello", "/login").permitAll()
+             .anyRequest().authenticated())
+        .formLogin(login -> 
+            login.loginPage("/login"));
+        
+        return http.build();
+    }    
+}
