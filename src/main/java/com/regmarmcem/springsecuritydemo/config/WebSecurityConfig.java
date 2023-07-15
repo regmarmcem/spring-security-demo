86cbf4b430147efebb167c17eecae43f44dabeb0
regmarmcem <tknktknktknktknktknk@gmail.com>
2023-07-15 12:42:31 +0900

    [fix] add passwordEncoder

--- src/main/java/com/regmarmcem/springsecuritydemo/config/WebSecurityConfig.java
+++ src/main/java/com/regmarmcem/springsecuritydemo/config/WebSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,5 +21,10 @@ public class WebSecurityConfig {
             login.loginPage("/login"));
         
         return http.build();
-    }    
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
